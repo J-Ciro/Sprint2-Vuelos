7d668346e57e261aef7e090b836ada314e3335e4
natalia maldonado <108nrm@gmail.com>
2023-05-17 12:25:29 -0500

    página principal

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import FeatureHomePage from "./components/feature-homepage/Index";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const App = () => {
+      const [formValue, setFormValues]= useState({
+        travelRounded: null,
+        origin: null,
+        destiny: null,
+        dateArrive: null,
+        dateLeaved: null,
+        passengers:{ Adult: 0, child: 0, baby: 0},
+        code: ''
+      })
+
+
+    return(
+        <BrowserRouter>
+           <Routes>
+              <Route path="/" element={<FeatureHomePage formValue={formValue} setFormValues={setFormValues}/>} />
+           </Routes>
+        </BrowserRouter>
+    )
+}
+export default App;
