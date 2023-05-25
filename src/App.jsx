2ee747a6d2c58bcd84a0adf25e4bb0b3f3759357
J-Ciro <37349818+J-Ciro@users.noreply.github.com>
2023-05-24 21:55:22 -0500

    b

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Layout } from "./Components/Layout/Layout.jsx";
-import FeatureHomePage from "./Components/feature-homepage/Index";
+import FeatureHomePage from "./Components/feature-homepage/Index.jsx";
 import Flights from "./Components/Flights/Flights";
 import FlightSeats from "./Components/FlightSeats/FlightSeats";
 import NotFound from "./Components/NotFound/NotFound";
